5e945496a977049fa4146a87fca23b792436537b
preeti chanyal <preeti.chanyal@inorder.in>
2024-03-04 08:46:43 +0530

    Initial commit

---
+++ src/test/java/com/ahiresearch/identity/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahiresearch.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
